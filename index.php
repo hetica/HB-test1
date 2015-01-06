7a705c18b880af65546266af8620310d0b4d2088
root <root@lamp.rana.jp>
2015-01-06 12:07:56 +0100

    new

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-Hello World
+<?php
+	/*
+	 * la documentation
+	 * Les '*' ne sont pas obligatoires
+	 * mais ils sont habituels
+	 */
+	$prenom = "Benoit" ;
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Cours PHP</title>	
+	</head>
+	
+	<body>
+		<h1>Cours PHP</h1>
+		
+		<p>
+			<?php echo "Salut " . $prenom ; ?>
+		</p>
+	</body>
+</html>
