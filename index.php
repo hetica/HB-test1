78de23bdf2f2af16c733e6bb7af85101fc8a99f8
root <root@lamp.rana.jp>
2015-01-10 13:39:32 +0100

    TP_base php

--- index.php
+++ index.php
@@ -49,7 +49,44 @@
 				echo "<p>" . $personne['prenom'] . " a " . $personne['age'] . " ans</p>" ;
 			}
 			
+			echo "<h3>TP 5</h3>" ;			
+			$clients = array(
+				array('Monique', '55', 'f') ,
+				array('Jean', '23', 'm') ,
+				array('Frédéric', '45', 'm') ,
+				array('Noémie', '34', 'f') ,
+				array('Calypso', '20', 'f') ,
+				array('Titouan', '22', 'm') ,
+				array('Priscilla', '27', 'f') ,
+				array('Etienne', '56', 'm') ,
+				array('Amélie', '41', 'f') ,
+				) ;
+			//echo "<pre>" . print_r($clients) . "</pre>" ;
 			
+			foreach ($clients as $client)
+			{
+				if ($client[1] > 18 AND $client[1] < 41 AND $client[2] == "f" ) 
+				{
+					echo $client[0] . " est une ménagère de moins de 40 ans<br/><br/>" ;
+				}
+			}
+			
+			$cpt = 0 ;
+			do
+			{
+				$pair = rand(0, 999) ;
+				$impair1 = rand(0 , 999) ;
+				$impair2 = rand(0 , 999) ;
+				$cpt++ ;
+				$s = "" ;
+				if ($cpt > 1) { $s = "s" ; }
+				if ($pair % 2 == 0 AND $impair1 % 2 == 1 AND $impair2 % 2 == 1)
+				{
+					echo "<h3>TP 6</h3>" ;
+					echo $pair . "<br/>" . $impair1 . "<br/>" . $impair2 . "<br/>" ;
+					echo "<br/>Il a fallu " . $cpt . " coup" . $s . " pour y arriver" ;
+				}
+			} while ($pair % 2 != 0 OR $impair1 % 2 != 1 OR $impair2 % 2 != 1)
 			?>
 	</body>
 </html>
